0988d119eed4f5af8fa57f4c35a3ba51a669da02
zyx_cambridge@gmail.com <zyx_cambridge@gmail.com>
2016-12-27 11:15:18 +0800

    建造者模式

--- DPCode/建造者模式/buildsample.cpp
+++ DPCode/建造者模式/buildsample.cpp
@@ -1,62 +1,201 @@
+///*
+//** FileName     : BuilderPattern
+//** Author       : Jelly Young
+//** Date         : 2013/11/23
+//** Description  : More information, please go to http://www.jellythink.com
+//*/
+//
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//class Builder;
+//
+//// Product
+//class Product
+//{
+//public:
+//	void AddPart(const char *info) { m_PartInfoVec.push_back(info); }
+//	void ShowProduct()
+//	{
+//		for (std::vector<const char *>::iterator item = m_PartInfoVec.begin(); 
+//			item != m_PartInfoVec.end(); ++item)
+//		{
+//			cout<<*item<<endl;
+//		}
+//	}
+//
+//private:
+//	std::vector<const char *> m_PartInfoVec;
+//};
+//
+//// Builder
+//class Builder
+//{
+//public:
+//	virtual void BuildPartA() {}
+//	virtual void BuildPartB() {}
+//	virtual Product *GetProduct() { return NULL; }
+//};
+//
+//// ConcreteBuilder
+//class ConcreteBuilder : public Builder
+//{
+//public:
+//	ConcreteBuilder() { m_Product = new Product(); }
+//	void BuildPartA()
+//	{
+//		m_Product->AddPart("PartA completed");
+//	}
+//
+//	void BuildPartB()
+//	{
+//		m_Product->AddPart("PartB completed");
+//	}
+//
+//	Product *GetProduct() { return m_Product; }
+//
+//private:
+//	Product *m_Product;
+//};
+//
+//// Director
+//class Director
+//{
+//public:
+//	Director(Builder *builder) { m_Builder = builder; }
+//	void CreateProduct()
+//	{
+//		m_Builder->BuildPartA();
+//		m_Builder->BuildPartB();
+//	}
+//
+//private:
+//	Builder *m_Builder;
+//};
+//
+//// main
+//int main()
+//{
+//	Builder *builderObj = new ConcreteBuilder();
+//	Director directorObj(builderObj);
+//	directorObj.CreateProduct();
+//	Product *productObj = builderObj->GetProduct();
+//	if (productObj == NULL)
+//	{
+//		return 0;
+//	}
+//	productObj->ShowProduct();
+//
+//        delete productObj;
+//        productObj = NULL; // 谢谢宾零同学的review
+//	delete builderObj;
+//	builderObj = NULL;
+//}
+
+
+
+
+
+
+
+
+
+
+
 /*
 ** FileName     : BuilderPattern
 ** Author       : Jelly Young
-** Date         : 2013/11/23
+** Date         : 2013/11/22
 ** Description  : More information, please go to http://www.jellythink.com
 */
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class Builder;
+typedef enum MANTYPETag
+{
+	kFatMan,
+	kThinMan,
+	kNormal
+}MANTYPE;
 
-// Product
-class Product
+class Man
 {
 public:
-	void AddPart(const char *info) { m_PartInfoVec.push_back(info); }
-	void ShowProduct()
+	void SetHead(MANTYPE type){ m_Type = type; }
+	void SetBody(MANTYPE type){ m_Type = type; }
+	void SetLeftHand(MANTYPE type){ m_Type = type; }
+	void SetRightHand(MANTYPE type){ m_Type = type; }
+	void SetLeftFoot(MANTYPE type){ m_Type = type; }
+	void SetRightFoot(MANTYPE type){ m_Type = type; }
+	void ShowMan()
 	{
-		for (std::vector<const char *>::iterator item = m_PartInfoVec.begin(); 
-			item != m_PartInfoVec.end(); ++item)
+		switch (m_Type)
 		{
-			cout<<*item<<endl;
+		case kFatMan:
+			cout << "I'm a fat man" << endl;
+			return;
+
+		case kThinMan:
+			cout << "I'm a thin man" << endl;
+			return;
+
+		default:
+			cout << "I'm a normal man" << endl;
+			return;
 		}
 	}
 
 private:
-	std::vector<const char *> m_PartInfoVec;
+	MANTYPE m_Type;
 };
 
 // Builder
 class Builder
 {
 public:
-	virtual void BuildPartA() {}
-	virtual void BuildPartB() {}
-	virtual Product *GetProduct() { return NULL; }
+	virtual void BuildHead(){}
+	virtual void BuildBody(){}
+	virtual void BuildLeftHand(){}
+	virtual void BuildRightHand(){}
+	virtual void BuildLeftFoot(){}
+	virtual void BuildRightFoot(){}
+	virtual Man *GetMan(){ return NULL; }
 };
 
-// ConcreteBuilder
-class ConcreteBuilder : public Builder
+// FatManBuilder
+class FatManBuilder : public Builder
 {
 public:
-	ConcreteBuilder() { m_Product = new Product(); }
-	void BuildPartA()
-	{
-		m_Product->AddPart("PartA completed");
-	}
-
-	void BuildPartB()
-	{
-		m_Product->AddPart("PartB completed");
-	}
-
-	Product *GetProduct() { return m_Product; }
+	FatManBuilder(){ m_FatMan = new Man(); }
+	void BuildHead(){ m_FatMan->SetHead(kFatMan); }
+	void BuildBody(){ m_FatMan->SetBody(kFatMan); }
+	void BuildLeftHand(){ m_FatMan->SetLeftHand(kFatMan); }
+	void BuildRightHand(){ m_FatMan->SetRightHand(kFatMan); }
+	void BuildLeftFoot(){ m_FatMan->SetLeftFoot(kFatMan); }
+	void BuildRightFoot(){ m_FatMan->SetRightFoot(kFatMan); }
+	Man *GetMan(){ return m_FatMan; }
 
 private:
-	Product *m_Product;
+	Man *m_FatMan;
+};
+
+// ThisManBuilder
+class ThinManBuilder : public Builder
+{
+public:
+	ThinManBuilder(){ m_ThinMan = new Man(); }
+	void BuildHead(){ m_ThinMan->SetHead(kThinMan); }
+	void BuildBody(){ m_ThinMan->SetBody(kThinMan); }
+	void BuildLeftHand(){ m_ThinMan->SetLeftHand(kThinMan); }
+	void BuildRightHand(){ m_ThinMan->SetRightHand(kThinMan); }
+	void BuildLeftFoot(){ m_ThinMan->SetLeftFoot(kThinMan); }
+	void BuildRightFoot(){ m_ThinMan->SetRightFoot(kThinMan); }
+	Man *GetMan(){ return m_ThinMan; }
+
+private:
+	Man *m_ThinMan;
 };
 
 // Director
@@ -64,31 +203,38 @@ class Director
 {
 public:
 	Director(Builder *builder) { m_Builder = builder; }
-	void CreateProduct()
-	{
-		m_Builder->BuildPartA();
-		m_Builder->BuildPartB();
-	}
+	void CreateMan();
 
 private:
 	Builder *m_Builder;
 };
 
-// main
-int main()
+void Director::CreateMan()
 {
-	Builder *builderObj = new ConcreteBuilder();
-	Director directorObj(builderObj);
-	directorObj.CreateProduct();
-	Product *productObj = builderObj->GetProduct();
-	if (productObj == NULL)
-	{
-		return 0;
-	}
-	productObj->ShowProduct();
+	m_Builder->BuildHead();
+	m_Builder->BuildBody();
+	m_Builder->BuildLeftHand();
+	m_Builder->BuildRightHand();
+	m_Builder->BuildLeftHand();
+	m_Builder->BuildRightHand();
+}
 
-        delete productObj;
-        productObj = NULL; // 谢谢宾零同学的review
+int main(int argc, char *argv[])
+{
+	Builder *builderObj = new FatManBuilder();
+	Director directorObj(builderObj);
+	directorObj.CreateMan();
+	Man *manObj = builderObj->GetMan();
+	if (manObj == NULL)
+		return 0;
+
+	manObj->ShowMan();
+
+	delete manObj; // 感谢张小张同学的review
+	manObj = NULL;
+
 	delete builderObj;
 	builderObj = NULL;
-}
+
+	return 0;
+};
